ba2c066926d0a253409e0491f48ab5281a3c364d
Allison Kroboth <42.allison.k@gmail.com>
2022-02-12 07:34:06 -0500

    adding temperature challenge

---
+++ allison/temperature.py
@@ -0,0 +1,19 @@
+#converting fahrenheit to celsius
+celsius_100=(100-32)*(5/9)
+print(celsius_100)
+print(type(celsius_100))
+#the result is a float
+
+celsius_0=(0-32)*(5/9)
+print(celsius_0)
+
+print((34.2-32)*(5/9))
+
+#converting celsius to fahrenheit
+print(5*(9/5)+32)
+
+#which is higher 30.2C or 85.1F
+
+print(30.2*(9/5)+32)
+
+#30.2C is higher than 85.1F
