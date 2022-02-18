d9e5f250430f16b4378916e8a040891ab57a0a86
Blaine Reid <96265960+Blaine-Reid@users.noreply.github.com>
2022-02-17 18:10:31 -0700

    Update snippets_challenge.py

--- blaine/3_logic/snippets_challenge.py
+++ blaine/3_logic/snippets_challenge.py
@@ -66,7 +66,7 @@ print("Code Snippet 5:")
 m = "GOAT"
 n = "goat"
 
-o = (m == n)
+o = (m != n)
 
 print (f"The value of o ({o}) is True since Python is case-sensitive.")
 assert(o == True) #Do not change this line
