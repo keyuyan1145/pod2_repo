0fce8399fae85f9b28dc30bbaedd8b4fbfd19652
Blaine Reid <reid.blaine08@gmail.com>
2022-02-19 09:01:09 -0700

    test after ls-files -m, git reset --hard

--- blaine/test.py
+++ blaine/test.py
@@ -1,5 +1,9 @@
 def test(arr):
     '''Testing git push'''
     # akfjas
-    # Test without extensions
-# test after git clean -df
+    # Test without extensions -fail
+# test after git clean -df - fail
+
+# test after:
+# git ls-files -m                                                    
+#  git reset --hard blaine-test    
