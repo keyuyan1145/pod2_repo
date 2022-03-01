3d646bfd9b253dfefe19b17ce6aa9cbf39d3acf8
Allison Kroboth <42.allison.k@gmail.com>
2022-02-28 19:02:47 -0500

    commiting playlist stuff without bonus done

--- allison/week_4/importing/playlist_functions.py
+++ allison/week_4/importing/playlist_functions.py
@@ -24,7 +24,8 @@ TODO (Question 6): define a function called get_playlist_length()
 This function should have one parameter called 'playlist'
 The function should return an integer value indicating how many songs there are
 '''
-
+def get_playlist_length(playlist):
+	return len(playlist)
 
 
 
@@ -44,4 +45,6 @@ Assuming that the third track in your playlist 'Controversy' by 'Prince'
 This function should ALSO increase the 'plays' value for that song's dictionary by 1
 So, if 'Controversy' has 0 plays so far, it should now be increased to 1
 '''
-
+def play_track(playlist, track=1):
+	print(f'Now playing track {track}, {playlist[track-1]}, by {playlist[track-1]}')
+	playlist[track-1]['plays']+=track-1
