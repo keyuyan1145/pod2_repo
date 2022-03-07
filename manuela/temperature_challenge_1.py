f6b0cb956c516523b15abf5099b396a788bc5fdc
1mfarhi <manuelafarhi@sbcglobal.net>
2022-03-07 14:57:15 -0600

    updated and forgotten assignment hand in of temperature challenge

---
+++ manuela/temperature_challenge_1.py
@@ -0,0 +1,17 @@
+#Convert a temperature of 100 degrees fahrenheit to celsius
+celsius_100 = (100-32)*5/9
+print(celsius_100)
+#Convert a temperature of 0 degrees fahrenheit to celsius
+print(type(celsius_100))
+celsius_0 = (0-32)*5/9
+print(celsius_0)
+#Convert a temperature of 34.2 degrees fahrenheit to celsius
+ #* Do this one all in one print statement **without** saving any variables
+print((34.2-32)*5/9)
+### Now, can you convert back?
+
+#Convert a temperature of 5 degrees celsius to fahrenheit?
+print((5*9/5)+32)
+# What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
+#30.2 C is > 85.1F, it is 86.36 F
+print((30.2*9/5)+32)
