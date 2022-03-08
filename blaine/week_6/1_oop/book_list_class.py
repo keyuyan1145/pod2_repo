dc5a186ef6e12c975fc76012d0e31e6273588486
Blaine-Reid <reid.blaine08@gmail.com>
2022-03-07 17:41:15 -0700

    blaine wk6 oop challenge

--- blaine/week_6/1_oop/book_list_class.py
+++ blaine/week_6/1_oop/book_list_class.py
@@ -2,26 +2,33 @@ from book_class import Book
 
 
 class Booklist():
-	def __init__(self):
-		"""Initialize the empty book list"""
-		pass
+    def __init__(self):
+        """Initialize the empty book list"""
+        self.books = []
 
-	def add(self, title, author):
-		"""Add a Book object with the given title and author to the book list"""
-		pass
+    def add(self, title, author):
+        """Add a Book object with the given title and author to the book list"""
+        book = {'title': title, 'author': author}
+        self.books.append(book)
 
-	def count_books(self):
-		"""Return the number of books currently in the booklist"""
-		pass
+    def count_books(self):
+        """Return the number of books currently in the booklist"""
+        return len(self.books)
 
-	def remove_title(self, title):
-		"""Remove a book from the book list"""
-		pass
+    def remove_title(self, title):
+        """Remove a book from the book list"""
+        self.books = [
+            book for book in self.books if book['title'].lower() != title.lower()]
 
-	def display_titles(self):
-		"""Print out all titles currently in the book list"""
-		pass
+    def display_titles(self):
+        """Print out all titles currently in the book list"""
+        sorted_books = sorted(
+            self.books, key=lambda book: book['title'])
+        alpha_books = [book['title'] for book in sorted_books]
+        print(alpha_books)
 
-	def is_empty(self):
-		"""Return True if the book list is empty, False if not"""
-		pass
+    def is_empty(self):
+        """Return True if the book list is empty, False if not"""
+        if self.books:
+            return False
+        return True
