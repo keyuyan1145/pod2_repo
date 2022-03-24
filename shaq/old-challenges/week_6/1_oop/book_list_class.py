30ca4aa757320035ccf6c1c22754fd7ee0cfcc7b
bronji <shaquille.roberts6@gmail.com>
2022-03-24 15:27:13 -0400

    book_list_class

--- shaq/old-challenges/week_6/1_oop/book_list_class.py
+++ shaq/old-challenges/week_6/1_oop/book_list_class.py
@@ -1,27 +1,33 @@
+from turtle import title
 from book_class import Book
 
 
 class Booklist():
 	def __init__(self):
 		"""Initialize the empty book list"""
-		pass
+		self.books = []
+		
 
 	def add(self, title, author):
 		"""Add a Book object with the given title and author to the book list"""
-		pass
+		book = Book(title, author)
+		self.books.append(book)
 
 	def count_books(self):
 		"""Return the number of books currently in the booklist"""
-		pass
+		return len(self.books)
 
 	def remove_title(self, title):
 		"""Remove a book from the book list"""
-		pass
+		for book in self.books:
+			if book.title == title:
+				self.books.remove(book)
 
 	def display_titles(self):
 		"""Print out all titles currently in the book list"""
-		pass
+		print(sorted([book.title for book in self.books]))
+		
 
 	def is_empty(self):
 		"""Return True if the book list is empty, False if not"""
-		pass
+		return len(self.books) == 0
